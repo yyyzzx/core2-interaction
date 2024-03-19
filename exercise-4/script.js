52591c8390dfeccfa457b6154ecd655bd434e245
yyyzzx <yej005@newschool.edu>
2024-03-19 19:40:56 -0400

    ex4

---
+++ exercise-4/script.js
@@ -0,0 +1,29 @@
+let V1 = ["wobble", "salsa", "moonwalk", "giggle"];
+let randomV1 = Math.floor(Math.random() * V1.length);
+let verb1 = document.getElementById("verb1");
+verb1.innerHTML = V1[randomV1];
+
+let V2 = ["eat", "smash", "toss"];
+let randomV2 = Math.floor(Math.random() * V2.length);
+let verb2 = document.getElementById("verb2");
+verb2.innerHTML = V2[randomV2];
+
+let V3 = ["sob", "yell", "moan", "growl"];
+let randomV3 = Math.floor(Math.random() * V3.length);
+let verb3 = document.getElementById("verb3");
+verb3.innerHTML = V3[randomV3];
+
+let A1 = ["hesitant", "stubborn", "reluctant"];
+let randomA1 = Math.floor(Math.random() * A1.length);
+let adj1 = document.getElementById("adj1");
+adj1.innerHTML = A1[randomA1];
+
+let N1 = ["adventure", "puzzle", "thriller", "horror game", "quest"];
+let randomN1 = Math.floor(Math.random() * N1.length);
+let noun1 = document.getElementById("noun1");
+noun1.innerHTML = N1[randomN1];
+
+let A2 = ["deep", "bold", "brilliant", "dazzling", "bloody"];
+let randomA2 = Math.floor(Math.random() * A2.length);
+let adj2 = document.getElementById("adj2");
+adj2.innerHTML = A2[randomA2];
