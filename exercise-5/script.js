fddb9e3cff8ae3345483194b0666f5ba3049450f
yyyzzx <yej005@newschool.edu>
2024-03-28 23:43:55 -0400

    ex-5

---
+++ exercise-5/script.js
@@ -0,0 +1,75 @@
+let box1 = document.getElementById("box1");
+let box2 = document.getElementById("box2");
+let box3 = document.getElementById("box3");
+let box4 = document.getElementById("box4");
+let box5 = document.getElementById("box5");
+let box6 = document.getElementById("box6");
+let box7 = document.getElementById("box7");
+let box8 = document.getElementById("box8");
+let box9 = document.getElementById("box9");
+
+let colors = ["#122340", "#2c6c63", "#3da1b0", "#4bb3b5", "#a6d1d5"];
+let borders = ["ridge", "solid", "groove", "inset", "outset"];
+
+let randomColor1 = Math.floor(Math.random() * colors.length);
+let randomColor11 = Math.floor(Math.random() * colors.length);
+let randomBorder1 = Math.floor(Math.random() * borders.length);
+box1.style.backgroundColor = colors[randomColor1];
+box1.style.border = "15px " + borders[randomBorder1] + colors[randomColor11];
+box1.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
+
+let randomColor2 = Math.floor(Math.random() * colors.length);
+let randomColor22 = Math.floor(Math.random() * colors.length);
+let randomBorder2 = Math.floor(Math.random() * borders.length);
+box2.style.backgroundColor = colors[randomColor2];
+box2.style.border = "15px " + borders[randomBorder2] + colors[randomColor22];
+box2.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
+
+let randomColor3 = Math.floor(Math.random() * colors.length);
+let randomColor33 = Math.floor(Math.random() * colors.length);
+let randomBorder3 = Math.floor(Math.random() * borders.length);
+box3.style.backgroundColor = colors[randomColor3];
+box3.style.border = "15px " + borders[randomBorder3] + colors[randomColor33];
+box3.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
+
+let randomColor4 = Math.floor(Math.random() * colors.length);
+let randomColor44 = Math.floor(Math.random() * colors.length);
+let randomBorder4 = Math.floor(Math.random() * borders.length);
+box4.style.backgroundColor = colors[randomColor4];
+box4.style.border = "15px " + borders[randomBorder4] + colors[randomColor44];
+box4.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
+
+let randomColor5 = Math.floor(Math.random() * colors.length);
+let randomColor55 = Math.floor(Math.random() * colors.length);
+let randomBorder5 = Math.floor(Math.random() * borders.length);
+box5.style.backgroundColor = colors[randomColor5];
+box5.style.border = "15px " + borders[randomBorder5] + colors[randomColor55];
+box5.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
+
+let randomColor6 = Math.floor(Math.random() * colors.length);
+let randomColor66 = Math.floor(Math.random() * colors.length);
+let randomBorder6 = Math.floor(Math.random() * borders.length);
+box6.style.backgroundColor = colors[randomColor6];
+box6.style.border = "15px " + borders[randomBorder6] + colors[randomColor66];
+box6.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
+
+let randomColor7 = Math.floor(Math.random() * colors.length);
+let randomColor77 = Math.floor(Math.random() * colors.length);
+let randomBorder7 = Math.floor(Math.random() * borders.length);
+box7.style.backgroundColor = colors[randomColor7];
+box7.style.border = "15px " + borders[randomBorder7] + colors[randomColor77];
+box7.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
+
+let randomColor8 = Math.floor(Math.random() * colors.length);
+let randomColor88 = Math.floor(Math.random() * colors.length);
+let randomBorder8 = Math.floor(Math.random() * borders.length);
+box8.style.backgroundColor = colors[randomColor8];
+box8.style.border = "15px " + borders[randomBorder8] + colors[randomColor88];
+box8.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
+
+let randomColor9 = Math.floor(Math.random() * colors.length);
+let randomColor99 = Math.floor(Math.random() * colors.length);
+let randomBorder9 = Math.floor(Math.random() * borders.length);
+box9.style.backgroundColor = colors[randomColor9];
+box9.style.border = "15px " + borders[randomBorder9] + colors[randomColor99];
+box9.style.transform = "scale(" + Math.random() * ((1 - 0.95 + 1) + 0.95) + ")";
