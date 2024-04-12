68e36e09aab176e8a0f6433707df1c15f42ce53a
yyyzzx <yej005@newschool.edu>
2024-04-11 22:20:31 -0400

    ex-7

---
+++ exercise-7/script.js
@@ -0,0 +1,49 @@
+let textDisplay = document.getElementById("textDisplay");
+
+//change text
+let textInput = document.getElementById("specimen");
+function changeText() {
+    textDisplay.innerHTML = textInput.value;
+    textInput.style.color = "var(--grey)";
+}
+
+//change width
+let widthImput = document.getElementById("width");
+let widthValue = document.getElementById("widthValue");
+function changeWidth() {
+    textDisplay.style.fontStretch = widthImput.value + "%";
+    widthValue.innerHTML = "Width: " + widthImput.value + "%";
+}
+
+//change size
+let sizeInput = document.getElementById("size");
+let sizeValue = document.getElementById("sizeValue");
+function changeSize() {
+    textDisplay.style.fontSize = sizeInput.value + "pt";
+    sizeValue.innerHTML = "Font size: " + sizeInput.value + "pt";
+}
+
+//aalt
+function alternative() {
+    textDisplay.classList.toggle("aalt");
+}
+
+//italic
+function italic() {
+    textDisplay.classList.toggle("italic");
+}
+
+//decenders
+function decenders() {
+    textDisplay.classList.toggle("decenders");
+}
+
+//lowercases
+function lowercases() {
+    textDisplay.classList.toggle("lowercases");
+}
+
+//short
+function short() {
+    textDisplay.classList.toggle("short");
+}
